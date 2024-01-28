d49e8e63e6644696edbee730ca4062683edab425
RafaelSantosPereira <rafael.ocyan@gmail.com>
2024-01-28 23:24:50 +0000

    api-get-popular_movies

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
-const { app, BrowserWindow, Menu} = require('electron');
-const path = require('path');
+const isElectron = typeof require === 'function';
 
-let mainWindow
+if (isElectron) {
+  const { app, BrowserWindow, Menu} = require('electron');
+  const path = require('path');
 
-app.on('ready', () =>
-{
+  let mainWindow;
+
+  app.on('ready', () => {
     Menu.setApplicationMenu(null);
     mainWindow = new BrowserWindow({
-        // frame:false,
-        //titleBarStyle:'hidden',//permite esconder a barra de titulo, mantendo as funcionalidades da mesma
+      width: 1400,
+      height: 800,
+    });
 
-        //titleBarOverlay:{//permite customizar a barra(sõ funciona caso a barra não seja a padrão)
-            //color: '#1C1C1C',
-            //symbolColor: '#D8D8D8',
-            //height: 30
-       // }
-       width : 1400,
-       height:800,
-
-
-
-    })
-   
-    // Obtém o caminho completo para o ícone(o join junta cada um dos nomes para formar um caminho)
     const iconPath = path.join(__dirname, 'assets', 'images', 'logo.ico');
-
-    // Define o ícone da janela principal
     mainWindow.setIcon(iconPath);
-    mainWindow.loadURL(`file:${__dirname}/index.html `)//inicia o html
+    mainWindow.loadURL(`file:${__dirname}/index.html`);
 
+    // Chame aqui a lógica específica do Electron, se necessário
     
+  });
+} else {
+  // Chame aqui a lógica específica do navegador
+  const api_key = 'a5d66f53cd4d37e6c21ce410122b6b32';
+  const ImageBaseURL = 'https://image.tmdb.org/t/p/w500';
+  const base_url = 'https://api.themoviedb.org/3';
+  const api_url = base_url + '/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=' + api_key;
+  const movies_div = document.getElementById('slider-inner');
 
-});
+  getmovies(api_url);
+
+  function getmovies(url) {
+    fetch(url).then(res => res.json()).then(data => {
+      showMovies(data.results);
+    });
+  }
+
+  function showMovies(data) {
+    movies_div.innerHTML = '';
+    data.forEach(movie => {
+      const { title, poster_path, vote_average, release_date } = movie;
+      const year = release_date.substring(0, 4);
+      const movieEl = document.createElement('div');
+      movieEl.classList.add('movie-card');
+      movieEl.innerHTML = `
+        <a href="./detail.html" class="card-btn"> 
+          <figure class="poster-box card-banner">
+            <img src="${ImageBaseURL + poster_path}" class="img-cover" alt="" >
+          </figure>
+          <div class="card-wrapper">
+            <h4 class="title">${title}</h4>
+            <div class="meta-list">
+              <div class="meta-item">
+                <span class="span">${vote_average}</span>
+                <img src="./assets/images/star.png" width="20px" height="20px" loading="lazy" alt="rating">             
+              </div>
+              <div class="card-badge">${year}</div>           
+            </div>
+          </div>
+        </a>
+      `;
+      movies_div.appendChild(movieEl);
+    });
+  }
+}
+
+// Função que contém a lógica específica do Electron
+
+
